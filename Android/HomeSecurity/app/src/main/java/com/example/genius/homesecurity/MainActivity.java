b354ed5f9a3cbde8d087d91028a68c2600c5693a
Louis Dwy Sevrey Ompusunggu <if412010@students.del.ac.id>
2015-12-27 21:02:07 +0700

    Commit Beta Project

---
+++ Android/HomeSecurity/app/src/main/java/com/example/genius/homesecurity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.genius.homesecurity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.pushbots.push.Pushbots;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Pushbots.sharedInstance().init(this);
+    }
+}
